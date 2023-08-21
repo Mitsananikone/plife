55fcbef362304958ac748045d882897972eec308
Mit Sananikone <Mitsananikone@gmail.com>
2023-08-21 16:49:57 -0500

    Procedures page formated for all screen sizes

--- pages/procedures/page.jsx
+++ pages/procedures/page.jsx
@@ -2,6 +2,7 @@
 
 import React, { useRef, useEffect, useState } from "react";
 import Link from "next/link"; // Import the Link component
+import styles from "./procedurePage.module.css";
 
 export default function ProceduresCard() {
   const titleRef = useRef(null);
@@ -55,76 +56,75 @@ export default function ProceduresCard() {
     <div
       className="background"
       style={{
-    backgroundImage: "url('/images/procedures-legs.png')",
-  }}
->
-      <div
-        ref={containerRef}
-        className="bg-transparent w-2/5 mt-64 justify-center -ml-64 "
-      >
-        {" "}
-        {/* 60% width */}
-        <p className="text-xl font-bold text-gray-200 text-center ">
-          “You don&#39;t pay for your beauty surgery - You pay for your unique
-          experience“
-        </p>
-        <h1
-          ref={titleRef}
-          className="text-xl font-bold tracking-widest text-center mt-5 text-gray-200"
-          style={{ scale: "1.70" }}
-        >
+        backgroundImage: "url('/images/procedures-legs.png')",
+      }}
+    >
+      <div className={styles.procedureComponent}>
+        <div ref={containerRef} className={styles.proceduresHighlight}>
           {" "}
-          {/* Larger starting font */}
-          OUR PROCEDURES
-        </h1>
-        <div className="grid grid-cols-2 text-5xl mt-12 px-5 font-bold text-gray-400">
-          <p className={`text-left ${highlightRow === 0 ? "fade-in" : ""}`}>
-            BEAUTY
+          {/* 60% width */}
+          <p className={styles.headerText}>
+            “You don&#39;t pay for your beauty surgery - You pay for your unique
+            experience“
           </p>
-          <p className={`text-right ${highlightRow === 0 ? "fade-in" : ""}`}>
-            SURGERY
-          </p>
-          <p
-            className={`text-left mt-2 ${highlightRow === 1 ? "fade-in" : ""}`}
-          >
-            GENDER
-          </p>
-          <p
-            className={`text-right mt-2 ${highlightRow === 1 ? "fade-in" : ""}`}
-          >
-            TRANSITIONING
-          </p>
-          <p
-            className={`text-left mt-2 ${highlightRow === 2 ? "fade-in" : ""}`}
-          >
-            AESTHETIC
-          </p>
-          <p
-            className={`text-right mt-2 ${highlightRow === 2 ? "fade-in" : ""}`}
-          >
-            NON-SURGICAL
-          </p>
-          <p
-            className={`text-left mt-2 ${highlightRow === 3 ? "fade-in" : ""}`}
-          >
-            DENTAL
-          </p>
-          <p
-            className={`text-right mt-2 ${highlightRow === 3 ? "fade-in" : ""}`}
-          >
-            TREATMENTS
-          </p>
-        </div>
-        
-        <div className="flex justify-center cursor-pointer z-0">
-        <Link href="/procedures/type/page" passHref>
-            <button
-              className="bg-gray-300 opacity-60 text-black font-bold py-2 px-4 rounded whitespace-nowrap h-16 shadow-lg hover:shadow-xl transition-all mt-8 w-64 hover:text-gray-700 cursor-pointer"
-              style={{ borderRadius: "4px" }}
+          <h1 className={styles.mainTitle} >
+            OUR PROCEDURES
+          </h1>
+          <div className={styles.highlightText}>
+            <p className={`text-left ${highlightRow === 0 ? "fade-in" : ""}`}>
+              BEAUTY
+            </p>
+            <p className={`text-right ${highlightRow === 0 ? "fade-in" : ""}`}>
+              SURGERY
+            </p>
+            <p
+              className={`text-left mt-2 ${
+                highlightRow === 1 ? "fade-in" : ""
+              }`}
             >
-              View Details
-            </button>
-          </Link>
+              GENDER
+            </p>
+            <p
+              className={`text-right mt-2 ${
+                highlightRow === 1 ? "fade-in" : ""
+              }`}
+            >
+              TRANSITIONING
+            </p>
+            <p
+              className={`text-left mt-2 ${
+                highlightRow === 2 ? "fade-in" : ""
+              }`}
+            >
+              AESTHETIC
+            </p>
+            <p
+              className={`text-right mt-2 ${
+                highlightRow === 2 ? "fade-in" : ""
+              }`}
+            >
+              NON-SURGICAL
+            </p>
+            <p
+              className={`text-left mt-2 ${
+                highlightRow === 3 ? "fade-in" : ""
+              }`}
+            >
+              DENTAL
+            </p>
+            <p
+              className={`text-right mt-2 ${
+                highlightRow === 3 ? "fade-in" : ""
+              }`}
+            >
+              TREATMENTS
+            </p>
+          </div>
+          <div className={styles.detailsButtonContainer}>
+            <Link href="/procedures/type/page" passHref>
+              <button className={styles.detailsButton}>View Details</button>
+            </Link>
+          </div>
         </div>
       </div>
     </div>
--- pages/procedures/type/page.jsx
+++ pages/procedures/type/page.jsx
@@ -2,11 +2,12 @@
 
 import React, { useEffect } from 'react';
 import TreatmentCard from "@/components/treatmentCard/treatmentCard";
+import styles from './type.module.css';
 
 export default function Treatments() {
   useEffect(() => {
     // Set the background color when the component mounts
-    document.body.style.backgroundColor = 'bg-gray-200';
+    document.body.style.backgroundColor = '#E5E7EB';
 
     return () => {
       // Reset the background color when the component unmounts
@@ -14,20 +15,17 @@ export default function Treatments() {
     };
   }, []);
   return (
-    <div className="flex flex-col items-center space-y-8 px-4 bg-gray-200 pb-32 h-100 w-screen overflow-hidden"> {/* Added overflow-hidden here */}
-      <h1
-        className="text-xl font-bold tracking-widest text-center mt-5 text-gray-500"
-        style={{ scale: "1.70" }}
-      >
+    <div className={styles.procedureContainer}> {/* Added overflow-hidden here */}
+      <h1>
         VIEW PROCEDURES AND TREATMENTS
       </h1>
-      <div className="flex flex-row justify-between space-x-2 md:space-x-4 lg:space-x-8 xl:space-x-12 2xl:space-x-16">
+      <div className={styles.tCardGroup}>
         <TreatmentCard imageUrl="/images/types/aesthetic.PNG" description="AESTHETIC & NON-SURGICAL" />
         <TreatmentCard imageUrl="/images/types/weightloss.PNG" description="WEIGHT LOSS" />
         <TreatmentCard imageUrl="/images/types/cosmeticsurgery.PNG" description="COSMETIC SURGERY" />
         <TreatmentCard imageUrl="/images/types/gyno.PNG" description="COSMETIC GYNECOLOGY" />
       </div>
-      <div className="flex flex-row justify-between space-x-2 md:space-x-4 lg:space-x-8 xl:space-x-12 2xl:space-x-16">
+      <div className={styles.tCardGroup}>
         <TreatmentCard imageUrl="/images/types/dental.PNG" description="DENTAL TREATMENTS" />
         <TreatmentCard imageUrl="/images/types/trans.PNG" description="GENDER TRANSITION" />
         <TreatmentCard imageUrl="/images/types/lipo.PNG" description="LIPOSUCTION" />
