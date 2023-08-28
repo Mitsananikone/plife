c89e46c5bcd80eead73982b6d8a4d5c6cbfe60a1
Mit Sananikone <Mitsananikone@gmail.com>
2023-08-28 13:40:33 -0500

    CONTACTS in navbar is now a text effect

--- components/nav/nav.jsx
+++ components/nav/nav.jsx
@@ -3,6 +3,7 @@
 import { useState } from "react";
 import Link from "next/link";
 import styles from "./nav.module.css";
+import ContactsShine from '@/components/contacts/contacts';
 
 const Navbar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -15,7 +16,7 @@ const Navbar = () => {
     { name: "QUOTE", link: "#quote" },
     { name: "PRICING", link: "#pricing" },
     { name: "PROMO", link: "#promo" },
-    { name: "CONTACTS", link: "#contacts" }, // Add this line
+    { name: "CONTACTS", link: "#contacts" }, 
   ];
 
   return (
@@ -31,22 +32,20 @@ const Navbar = () => {
         <div className={styles.hamburger}>
           {menuItems.map((item, index) => (
             <Link
-              href={
-                item.link ||
-                `#${item.name?.toLowerCase() || item.toLowerCase()}`
-              }
+              href={item.link || `#${item.name?.toLowerCase() || item.toLowerCase()}`}
               key={index}
               passHref
             >
               <span
-                className={`${styles.navItem} 
-              ${
-                item.name === "CONTACTS"
-                  ? `${styles.contacts} ${styles.sign} ${styles.link} ${styles.ledGlow}`
-                  : ""
-              }`}
+                className={styles.navItem}
+                onMouseOver={(e) => {
+                  e.target.style.textShadow = "2px 2px 4px #2119B4";
+                }}
+                onMouseOut={(e) => {
+                  e.target.style.textShadow = "1px 1px 2px rgba(0,0,0,0.3)";
+                }}
               >
-                {item.name || item}
+                {item.name === "CONTACTS" ? <ContactsShine  /> : item.name}
               </span>
             </Link>
           ))}
@@ -56,9 +55,9 @@ const Navbar = () => {
           className="md:hidden"
           onClick={() => setIsMenuOpen(!isMenuOpen)}
         >
-          <div className="h-1 w-6 mb-1 bg-white"></div>
-          <div className="h-1 w-6 mb-1 bg-white"></div>
-          <div className="h-1 w-6 bg-white"></div>
+          <div className=".row-span-1 w-6 mb-1 bg-white"></div>
+          <div className=".row-span-1 w-6 mb-1 bg-white"></div>
+          <div className=".row-span-1 w-6 bg-white"></div>
         </button>
       </div>
       {isMenuOpen && (
@@ -66,17 +65,11 @@ const Navbar = () => {
           {menuItems.map((item, index) => (
             <Link
               key={index}
-              href={
-                item.link ||
-                `#${item.name?.toLowerCase() || item.toLowerCase()}`
-              }
+              href={item.link || `#${item.name?.toLowerCase() || item.toLowerCase()}`}
               passHref
-              legacyBehavior
             >
               <span
-                className={`${styles.navItem} ${
-                  item.name === "CONTACTS" ? styles.ledGlow : ""
-                }`}
+                className={styles.navItem}
                 onMouseOver={(e) => {
                   e.target.style.textShadow = "2px 2px 4px #2119B4";
                 }}
@@ -84,7 +77,7 @@ const Navbar = () => {
                   e.target.style.textShadow = "1px 1px 2px rgba(0,0,0,0.3)";
                 }}
               >
-                {item.name || item}
+                {item.name === "CONTACTS" ? <ContactsShine  /> : item.name}
               </span>
             </Link>
           ))}
